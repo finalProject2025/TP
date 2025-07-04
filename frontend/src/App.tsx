5aa378e61e5cb0807943f221ee2a089fef598f7f
Ömer Akgül <oemer.akguel@gmx.de>
2025-07-04 09:17:38 +0200

    added app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,21 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Landing from './pages/Landing';
-import './App.css'
+import SimplePostsPage from './components/SimplePostsPage';
+import MessagesPage from './components/MessagesPage';
+import { ToastProvider } from './hooks/useToast';
 
 function App() {
-    return (
-    <Router>
+  return (
+    <ToastProvider>
+      <Router>
         <Routes>
           <Route path="/" element={<Landing />} />
+          <Route path="/posts" element={<SimplePostsPage />} />
+          <Route path="/messages" element={<MessagesPage />} />
         </Routes>
       </Router>
+    </ToastProvider>
   );
 }
 
-export default App
+export default App;
